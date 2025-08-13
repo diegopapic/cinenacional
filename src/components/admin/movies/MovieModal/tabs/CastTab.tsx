473b22f292b01249cd2d4fe3aee0ed3ce8dc7bf5
diego.papic <diego.papic@gmail.com>
2025-08-13 19:22:35 -0300

    fix: agregué onScreeningVenuesChange a MovieFormEnhanced en las otras pestañas

--- src/components/admin/movies/MovieModal/tabs/CastTab.tsx
+++ src/components/admin/movies/MovieModal/tabs/CastTab.tsx
@@ -22,6 +22,7 @@ export default function CastTab({
       onProductionCompaniesChange={() => {}}
       onDistributionCompaniesChange={() => {}}
       onThemesChange={() => {}}
+      onScreeningVenuesChange={() => { }}
       initialData={movieFormInitialData}
       showOnlyCast={true}
     />
